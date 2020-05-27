d60c68e2876fee9eaeff6a4b65ce425278193175
孔少宏 <kongshaohong@szbit.cn>
2020-05-27 17:20:17 +0800

    增加导航编码

---
+++ ksh/src/main/scala/cn/sibat/ksh/rl/DeepSarsa.scala
@@ -0,0 +1,187 @@
+package cn.sibat.ksh.rl
+
+import java.io.File
+
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
+import org.deeplearning4j.nn.graph.ComputationGraph
+import org.deeplearning4j.nn.weights.WeightInit
+import org.nd4j.linalg.activations.Activation
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.linalg.learning.config.Adam
+import org.nd4j.linalg.lossfunctions.LossFunctions
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+/**
+  * Created by kong at 2020/5/27
+  */
+class DeepSarsa(env: Environment) extends RL {
+  private val load_model = false
+  private val action_size = env.getAction.size
+  private val state_size = env.getInitState.getDimension
+  private val discount_factor = 0.99
+  private val learning_rate = 0.001
+  private var epsilon = 1.0
+  private val epsilon_decay = 0.9999
+  private val epsilon_min = 0.01
+  private var model: ComputationGraph = _
+
+  /**
+    * 预测action的概率
+    *
+    * @param input 环境输入
+    * @return
+    */
+  def predict(input: INDArray): Array[INDArray] = {
+    model.output(false, input)
+  }
+
+  /**
+    * 建立与初始化模型
+    * 如果加载训练好的模型则输入model_path,否则不需要
+    *
+    * @param model_path 模型地址
+    */
+  def build_model(model_path: String = "null"): Unit = {
+    if (load_model) {
+      model = ComputationGraph.load(new File(""), false)
+    } else {
+      val conf = new NeuralNetConfiguration.Builder()
+        .updater(new Adam(learning_rate))
+        .weightInit(WeightInit.XAVIER)
+        .graphBuilder()
+        .addInputs("input")
+        .addLayer("d1", new DenseLayer.Builder().nOut(30).nIn(state_size).activation(Activation.RELU).build(), "input")
+        .addLayer("d2", new DenseLayer.Builder().nOut(30).nIn(30).activation(Activation.RELU).build(), "d1")
+        .addLayer("d3", new OutputLayer.Builder(LossFunctions.LossFunction.MSE).nOut(action_size).nIn(30).build(), "d2")
+        .setOutputs("d3")
+        .build()
+      model = new ComputationGraph(conf)
+    }
+    model.init()
+  }
+
+  /**
+    * 训练模型
+    *
+    * @param state       当前状态
+    * @param action      当前动作
+    * @param reward      反馈
+    * @param next_state  下一状态
+    * @param next_action 下一动作
+    * @param done        是否结束
+    */
+  def train_model(state: State, action: Int, reward: Double, next_state: State, next_action: Int, done: Boolean): Unit = {
+    if (epsilon > epsilon_min)
+      epsilon *= epsilon_decay
+
+    val input = Nd4j.create(state.currentState).reshape(1, state.currentState.length)
+    val target = model.output(input).head
+    if (done)
+      target.putScalar(action, reward)
+    else {
+      val next_state_vec = Nd4j.create(next_state.currentState).reshape(1, next_state.currentState.length)
+      val value = model.output(next_state_vec).head.getDouble(next_action.toLong)
+      target.putScalar(action, reward + discount_factor * value)
+    }
+
+    model.fit(Array(input), Array(target))
+  }
+
+  /**
+    * 获取当前状态的最大价值的动作
+    *
+    * @param state 状态
+    * @return
+    */
+  override def get_action(state: State): Int = {
+    var action = 0
+    if (Random.nextDouble() < epsilon) {
+      action = Random.nextInt(action_size)
+    } else {
+      val input = Nd4j.create(state.currentState).reshape(1, state.currentState.length)
+      val q_values = model.output(false, input)
+      action = q_values.head.argMax(1).getNumber().intValue()
+    }
+    action
+  }
+
+  /**
+    * 执行策略
+    *
+    */
+  override def move_by_policy(log: Boolean): Unit = {
+    var start = env.getInitState
+    var count = 0
+    val moves = new ArrayBuffer[String]()
+    var reward = 0.0
+    var done = true
+    while (done) {
+      val action = get_action(start)
+      val next_state = env.state_after_action(start, action)
+      count += 1 //up,right,down,left
+      val out = env.getAction.action_name(action)
+      if (log)
+        moves += out
+      reward = env.get_reward(start)
+      val next_action = get_action(next_state)
+      if (reward > 0)
+        done = false
+      train_model(start, action, reward, next_state, next_action, !done)
+      start = next_state
+    }
+    if (log)
+      println(s"policy:move-$count,moves:${moves.mkString(",")},reward:$reward")
+  }
+
+  def move_by_policy2(log: Boolean): Unit = {
+    var start = env.getInitState
+    var count = 0
+    val moves = new ArrayBuffer[String]()
+    val states = new ArrayBuffer[State]()
+    var reward = 0.0
+    while (env.asInstanceOf[MultiODEnv].ods.nonEmpty) {
+      val action = get_action(start)
+      val next_state = env.state_after_action(start, action)
+      count += 1 //up,right,down,left
+      val out = env.getAction.action_name(action)
+      if (log) {
+        moves += out
+        states += start
+      }
+      reward = env.get_action_reward(start, action)
+      val next_action = get_action(next_state)
+      val done = env.asInstanceOf[MultiODEnv].ods.isEmpty
+      train_model(start, action, reward, next_state, next_action, done)
+      start = next_state
+    }
+    if (log) {
+      println(s"policy:move-$count,moves:${moves.mkString(",")},reward:$reward")
+      println(states.mkString(";"))
+    }
+    moves.clear()
+  }
+}
+
+object DeepSarsa {
+  def main(args: Array[String]): Unit = {
+    val env = new MultiODWithDetectEnv(5, 5, 1)
+    val ods = Array((0, 1, 1, 3, 2), (1, 2, 2, 1, 3), (2, 4, 0, 0, 4))
+    env.setODS(ods)
+    val sarsa = new DeepSarsa(env)
+    sarsa.build_model()
+    println("before improve:")
+    sarsa.move_by_policy2(true)
+    for (i <- 0 to 10000) {
+      env.reset(ods)
+      sarsa.move_by_policy2(false)
+      print(s"\rexec:$i")
+    }
+    println("after improve:")
+    env.reset(ods)
+    sarsa.move_by_policy2(true)
+  }
+}
--- ksh/src/main/scala/cn/sibat/ksh/rl/ODEnv.scala
+++ ksh/src/main/scala/cn/sibat/ksh/rl/ODEnv.scala
@@ -1,5 +1,6 @@
 package cn.sibat.ksh.rl
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 trait MultiOD {
@@ -288,6 +289,8 @@ class MultiODWithDetectEnv(width: Int, height: Int, detect: Int) extends Environ
     ods ++= arr
   }
 
+  def get_total_reward: Double = drop.length * 1.0
+
   /**
     * 获得当前状态下的反馈
     *
@@ -365,6 +368,175 @@ class MultiODWithDetectEnv(width: Int, height: Int, detect: Int) extends Environ
     for (i <- s.length until newVec.length) {
       newVec(i) = neighbor(i - s.length)
     }
+    newState
+  }
+
+  /**
+    * 检查当前状态的合理性，并转换为合理状态
+    *
+    * @param vec 当前状态
+    * @return
+    */
+  override def check_boundary(vec: Array[Double]): State = {
+    vec(0) = math.min(math.max(vec(0), 0), width - 1)
+    vec(1) = math.min(math.max(vec(1), 0), height - 1)
+    new State(vec)
+  }
+}
+
+/**
+  * 以state为中心向周边做+1的detect，形成状态
+  * 增加导航功能，一共4个方向，每个方向有5中状态
+  * 编码：当前坐标（2）,侦测范围（（1+detect*2）*（1+detect*2）-1）,导航（4）
+  * 即编码长度：5+（1+detect*2）*（1+detect*2）
+  * (-1.0, -0.5, 0.0, 0.5, 1.0)全O,有OD偏O,无OD,有OD偏D,全D
+  * Created by kong at 2020/5/18
+  */
+class MultiODWithDetectAndDirectEnv(width: Int, height: Int, detect: Int) extends Environment with MultiOD {
+  private val actions = Array(Array(0, -1), Array(0, 1), Array(-1, 0), Array(1, 0))
+  private val pick = new ArrayBuffer[Int]()
+  private val drop = new ArrayBuffer[Int]()
+  setInitState(new State(5 + (1 + 2 * detect) * (1 + 2 * detect))) //编码：当前坐标,侦测范围,导航
+  setAction(new Action(Array("l", "r", "u", "d")))
+
+  /**
+    * 重新开始
+    *
+    * @param arr Array
+    */
+  def reset(arr: Array[(Int, Int, Int, Int, Int)]): Unit = {
+    ods.clear()
+    ods ++= arr
+    pick.clear()
+    drop.clear()
+  }
+
+  /**
+    * 设置OD集
+    *
+    * @param arr od集
+    */
+  def setODS(arr: Array[(Int, Int, Int, Int, Int)]): Unit = {
+    ods ++= arr
+  }
+
+  def get_total_reward: Double = drop.length * 1.0
+
+  /**
+    * 获得当前状态下的反馈
+    *
+    * @param state 当前状态
+    * @return
+    */
+  override def get_reward(state: State): Double = {
+    var reward = 0.0
+    for (od <- ods) {
+      if (pick.isEmpty || !pick.contains(od._1)) {
+        if (state.currentState.head == od._2 && state.currentState(1) == od._3) {
+          pick += od._1
+          //          reward += 0.5
+        }
+      } else if (pick.contains(od._1) && state.currentState.head == od._4 && state.currentState(1) == od._5) {
+        reward += 1.0
+        pick.remove(pick.indexOf(od._1)) //到站
+        drop += od._1
+      }
+    }
+    drop.foreach(t => {
+      val index = ods.indexWhere(_._1 == t)
+      if (index != -1)
+        ods.remove(index)
+    })
+    reward
+  }
+
+
+  /**
+    * 获得当前状态下执行action的反馈
+    *
+    * @param state  当前状态
+    * @param action 动作
+    * @return
+    */
+  override def get_action_reward(state: State, action: Int): Double = {
+    val next_state = state_after_action(state, action)
+    get_reward(next_state)
+  }
+
+  /**
+    * 当前状态执行完action后的下一个状态
+    *
+    * @param state  当前状态
+    * @param action 动作
+    * @return
+    */
+  override def state_after_action(state: State, action: Int): State = {
+    val s = actions(action)
+    val vec = state.currentState.clone()
+    for (i <- s.indices) {
+      vec(i) += s(i)
+    }
+    val newState = check_boundary(vec)
+    val newVec = newState.currentState
+    val center = (newVec(0), newVec(1))
+    val coords = (-detect to detect).flatMap(t => (-detect to detect).map(s => (t, s))).toArray.filter(_ != (0, 0))
+    val neighbor = coords.map(t => {
+      val head = center._1 + t._1
+      val last = center._2 + t._2
+      if (head < 0 || last < 0) //超出边框
+        -10
+      else if (ods.exists(s => s._2 == head && s._3 == last)) { //o点
+        val id = ods.find(s => s._2 == head && s._3 == last).get
+        if (pick.contains(id._1)) //已经过
+          0
+        else
+          -1
+      } else if (ods.exists(s => s._4 == head && s._5 == last)) //d点
+        +1
+      else //空白点
+        0
+    })
+    //侦测信息
+    for (i <- s.length until newVec.length) {
+      newVec(i) = neighbor(i - s.length)
+    }
+    //方向信息
+    val map = new mutable.HashMap[String, Int]()
+    for (od <- ods) {
+      val o = (od._2 - center._1, od._3 - center._2)
+      val d = (od._4 - center._1, od._5 - center._2)
+      if (!pick.contains(od._1))
+        if (o._1 <= 0 && o._2 < 0) {
+          map.put(s"o-${newVec.length - 4}", map.getOrElse(s"o-${newVec.length - 4}", 0) + 1)
+        } else if (o._1 > 0 && o._2 <= 0) {
+          map.put(s"o-${newVec.length - 3}", map.getOrElse(s"o-${newVec.length - 3}", 0) + 1)
+        } else if (o._1 < 0 && o._2 >= 0) {
+          map.put(s"o-${newVec.length - 2}", map.getOrElse(s"o-${newVec.length - 2}", 0) + 1)
+        } else if (o._1 >= 0 && o._2 > 0) {
+          map.put(s"o-${newVec.length - 1}", map.getOrElse(s"o-${newVec.length - 1}", 0) + 1)
+        }
+
+      if (d._1 <= 0 && d._2 < 0) {
+        map.put(s"d-${newVec.length - 4}", map.getOrElse(s"d-${newVec.length - 4}", 0) + 1)
+      } else if (d._1 > 0 && d._2 <= 0) {
+        map.put(s"d-${newVec.length - 3}", map.getOrElse(s"d-${newVec.length - 3}", 0) + 1)
+      } else if (d._1 < 0 && d._2 >= 0) {
+        map.put(s"d-${newVec.length - 2}", map.getOrElse(s"d-${newVec.length - 2}", 0) + 1)
+      } else if (d._1 >= 0 && d._2 > 0) {
+        map.put(s"d-${newVec.length - 1}", map.getOrElse(s"d-${newVec.length - 1}", 0) + 1)
+      }
+    }
+    for (i <- newVec.length - 4 until newVec.length) {
+      if (map.getOrElse(s"o-$i", 0) > 0 && map.getOrElse(s"d-$i", 0) > 0) {
+        newVec(i) = if (map(s"o-$i").toFloat / map(s"d-$i") > 1.0) -0.5 else 0.5
+      } else if (map.getOrElse(s"o-$i", 0) > 0 && map.getOrElse(s"d-$i", 0) == 0) {
+        newVec(i) = -1.0
+      } else if (map.getOrElse(s"o-$i", 0) == 0 && map.getOrElse(s"d-$i", 0) > 0) {
+        newVec(i) = 1.0
+      } else
+        newVec(i) = 0.0
+    }
+
     newState
   }
 
